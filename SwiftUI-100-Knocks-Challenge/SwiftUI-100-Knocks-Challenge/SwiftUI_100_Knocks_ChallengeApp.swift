b490d4c7c067b624b57a32b01d1f6de0640593f6
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-05-01 13:33:21 +0900

    [Feat] 1/100: 画像をリサイズして表示(fit)

--- SwiftUI-100-Knocks-Challenge/SwiftUI-100-Knocks-Challenge/SwiftUI_100_Knocks_ChallengeApp.swift
+++ SwiftUI-100-Knocks-Challenge/SwiftUI-100-Knocks-Challenge/SwiftUI_100_Knocks_ChallengeApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_100_Knocks_ChallengeApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ContentView_001()
         }
     }
 }
---
+++ SwiftUI-100-Knocks-Challenge/SwiftUI-100-Knocks-Challenge/Views/001/ContentView_001.swift
@@ -0,0 +1,23 @@
+//
+//  001.swift
+//  SwiftUI-100-Knocks-Challenge
+//
+//  Created by af_nakandakari_toru on 2024/05/01.
+//
+
+import SwiftUI
+
+/// 画像をリサイズして表示(fit)
+struct ContentView_001: View {
+    var body: some View {
+        Image("cat001")
+            .resizable() // 画像をリサイズ可能にする。これをしないと画像サイズのまま表示しようとして 150x200 の大きさで表示できない
+            .scaledToFit()
+            .frame(width: 150, height: 200)
+            .background(.red)
+    }
+}
+
+#Preview {
+    ContentView_001()
+}
