a2dea61f8a0f171da3c7a7f4116c415d66821a2c
af_nakandakari_toru <nakandakari_toru@andfactory.co.jp>
2024-05-01 13:10:47 +0900

    Initial commit for Xcode project

---
+++ SwiftUI-100-Knocks-Challenge/SwiftUI-100-Knocks-Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUI-100-Knocks-Challenge
+//
+//  Created by af_nakandakari_toru on 2024/05/01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUI-100-Knocks-Challenge/SwiftUI-100-Knocks-Challenge/SwiftUI_100_Knocks_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_100_Knocks_ChallengeApp.swift
+//  SwiftUI-100-Knocks-Challenge
+//
+//  Created by af_nakandakari_toru on 2024/05/01.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_100_Knocks_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
